cae0a061da78414aecfd2e81e3529bd31a4ebe8c
nicolasclidorio7 <nicolas.clidorio@academico.ifpb.edu.br>
2024-12-13 17:53:50 +0000

    alterações01

--- main.py
+++ main.py
@@ -60,44 +60,45 @@ def q3():
 
     """
 
-    numero = int(input("Digite um número"))
+    numero = int(input("Digite um número: "))
 
     divisores = 0
 
-    for i in range(1,numero + 1):
+    for i in range(1, numero + 1):
         if numero % i == 0 and i % 3 == 0:
-            divisores += 1 
-        if divisores == 0:
-            print("o numero não possui divizores multiplos de 3")    
-        else:
-            print("Quantidade de divisores divididos por 3:")
+            divisores += 1
 
-            pass
+    if divisores == 0:
+        print("O número não possui divisores multiplos de 3")
+    else:
+        print(f"Quantidade de divisores divisiveis por 3: {divisores}")
+        
+    pass
 
 
 def q4():
     """
-Escreva um programa que receba como entrada dois números inteiros e retorne a soma dos números positivos no intervalo definido por eles, considerando inclusive os extremos.
+ Escreva um programa que receba como entrada dois números inteiros e retorne a soma dos números positivos no intervalo definido por eles, considerando inclusive os extremos.
 
     """
-numero1 = int(input(""))
-numero2 = int(input(""))
 
-soma = 0
+    numero1 = int(input(""))
+    numero2 = int(input(""))
 
-if numero1 < numero2:
-    for i in range(numero1, numero2 + 1):
-        if i > 0:
-            soma += i
-        else:
-            for i in range(numero2, numero1 + 1):
-              if i > 0:    
-                  soma += i
+    soma = 0
 
-        print(soma)
-        
-        pass
+    if numero1 < numero2:
+        for i in range(numero1, numero2 + 1):
+            if i > 0:
+                soma += i
+    else:
+        for i in range(numero2, numero1 + 1):
+            if i > 0:
+                soma += i
 
-    
+    print(soma)
+
+    pass
+
 
 
